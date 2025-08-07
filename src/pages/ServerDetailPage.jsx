40353a1cf8b7ba1ade0f8aa18a13bbbae755e1e9
venv <github@venv.email>
2025-08-07 10:59:38 -0300

    feat: Adiciona/Atualiza src/pages/ServerDetailPage.jsx

---
+++ src/pages/ServerDetailPage.jsx
@@ -0,0 +1,1 @@
+import React from 'react'; import { useParams } from 'react-router-dom'; function ServerDetailPage() { const { id } = useParams(); return (<div><h1>Server Details for {id}</h1></div>); } export default ServerDetailPage;
